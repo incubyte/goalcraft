d6524b5c2f5269292a37a28543e658f7c991bcae
Sahil Nayak <62712927+Nayak-Sahil@users.noreply.github.com>
2025-02-19 15:34:09 +0530

    Delete: Database Service
    Reason: Since we are using ORM, No need of it

--- okr-nest/src/app.module.ts
+++ okr-nest/src/app.module.ts
@@ -1,12 +1,11 @@
 import {Module} from '@nestjs/common';
 import {ObjectivesModule} from './objectives/objectives.module';
-import {DatabaseModule} from './database/database.module';
 import { PrismaService } from './prisma/prisma.service';
 import { KeyResultsModule } from './key-results/key-results.module';
 import { RagModule } from './rag/rag.module';
 
 @Module({
-    imports: [ObjectivesModule, DatabaseModule, KeyResultsModule, RagModule],
+    imports: [ObjectivesModule, KeyResultsModule, RagModule],
     controllers: [],
     providers: [PrismaService],
 })
