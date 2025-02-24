726d33bcef61fa63253d08caddb50c1516f7ef0e
Sahil Nayak <62712927+Nayak-Sahil@users.noreply.github.com>
2025-02-24 16:15:57 +0530

    Refactor(Message): Validation alert message

--- okr-client/src/components/AddKeyResultModal.tsx
+++ okr-client/src/components/AddKeyResultModal.tsx
@@ -30,7 +30,7 @@ export default function AddKeyResultModal({
     function handleAddKeyResult() {
         if (objectives === null) return;
 
-        if(keyResult.title === "") { alert("Please enter a field!"); return; }
+        if(keyResult.title === "") { alert("Title cannot be empty!"); return; }
 
         console.log(keyResult);
 
