b3883b737a400db9f4ccf44b64fbf4e2a87bc285
Sahil Nayak <62712927+Nayak-Sahil@users.noreply.github.com>
2025-03-03 13:16:30 +0530

    Fix(preStart.ts): Warning Message

--- okr-client/hooks/preStart.ts
+++ okr-client/hooks/preStart.ts
@@ -1,4 +1,4 @@
 if (process.env.npm_config_user_agent && !process.env.npm_config_user_agent.includes('pnpm')) {
-  console.error('⚠️  WARN: You must use pnpm to run the any command! \n');
+  console.error('⚠️  WARN: You must use pnpm to run the start command! \n');
   process.exit(1);
 }
