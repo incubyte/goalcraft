0bbad346c60ec2f4e11818a249e7e4819c4aa54c
Sahil Nayak <62712927+Nayak-Sahil@users.noreply.github.com>
2025-03-12 14:52:17 +0530

    Add(Zustand): react state management library

---
+++ okr-client/src/default/data.ts
@@ -0,0 +1,25 @@
+const defaultOKR = {
+  id: '',
+  objective: '',
+  keyResults: [
+    {
+      id: '',
+      title: '',
+      initialValue: 0,
+      currentValue: 0,
+      targetValue: 0,
+      metric: '',
+      objectiveId: '',
+    },
+  ],
+};
+
+const defaultKeyResult = {
+  title: '',
+  initialValue: 0,
+  currentValue: 0,
+  targetValue: 0,
+  metric: '',
+};
+
+export { defaultKeyResult, defaultOKR };
